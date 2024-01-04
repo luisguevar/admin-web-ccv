1b6a3b20f6a108161d54258ccb5fe209d181407f
Luis A. Guevara <guevaredo03@gmail.com>
2024-01-03 21:50:23 -0500

    update form productos

--- src/app/modules/products/add-new-product/add-new-product.component.ts
+++ src/app/modules/products/add-new-product/add-new-product.component.ts
@@ -11,49 +11,49 @@ import { ProductsService } from '../_services/products.service';
 })
 export class AddNewProductComponent implements OnInit {
 
-  isLoading$:any ;
+  isLoading$: any;
 
-  title:any = null;
-  sku:any = null;
-  categorie_id:any = '';
-  price_soles:any = '';
-  price_usd:any = '';
-  resumen:any = null;
-  description:any = null;
+  title: any = null;
+  sku: any = null;
+  categorie_id: any = '';
+  price_soles: any = '';
+  price_usd: any = '';
+  resumen: any = null;
+  description: any = null;
 
-  categories:any = [];
+  categories: any = [];
 
-  text:any = null;
-  tags:any = [];
+  text: any = null;
+  tags: any = [];
 
-  imagen_file:any = null;
-  imagen_previzualiza:any = null;
+  imagen_file: any = null;
+  imagen_previzualiza: any = null;
 
-  images_files:any = [];
-  img_files:any = null;
-  img_previzualizar:any = null;
+  images_files: any = [];
+  img_files: any = null;
+  img_previzualizar: any = null;
   constructor(
-    public toaster:Toaster,
+    public toaster: Toaster,
     public _productService: ProductsService,
   ) { }
 
   ngOnInit(): void {
     this.isLoading$ = this._productService.isLoading$;
-    this._productService.getInfo().subscribe((resp:any) => {
+    this._productService.getInfo().subscribe((resp: any) => {
       this.categories = resp.categories;
     })
   }
 
-  loadServices(){
+  loadServices() {
     this._productService.isLoadingSubject.next(true);
     setTimeout(() => {
       this._productService.isLoadingSubject.next(false)
     }, 50);
   }
 
-  processFile($event){
-    if($event.target.files[0].type.indexOf("image") < 0){
-      this.toaster.open(NoticyAlertComponent,{text:`danger-'EL ARCHIVO CARGADO NO ES UNA IMAGEN'`});
+  processFile($event) {
+    if ($event.target.files[0].type.indexOf("image") < 0) {
+      this.toaster.open(NoticyAlertComponent, { text: `danger-'EL ARCHIVO CARGADO NO ES UNA IMAGEN'` });
       return;
     }
     this.imagen_file = $event.target.files[0];
@@ -63,17 +63,17 @@ export class AddNewProductComponent implements OnInit {
     this.loadServices();
   }
 
-  addTags(){
+  addTags() {
     this.tags.push(this.text)
     this.text = null;
   }
-  removeTags(index){
-    this.tags.splice(index,1);
+  removeTags(index) {
+    this.tags.splice(index, 1);
   }
 
-  addFile($event){
-    if($event.target.files[0].type.indexOf("image") < 0){
-      this.toaster.open(NoticyAlertComponent,{text:`danger-'EL ARCHIVO CARGADO NO ES UNA IMAGEN'`});
+  addFile($event) {
+    if ($event.target.files[0].type.indexOf("image") < 0) {
+      this.toaster.open(NoticyAlertComponent, { text: `danger-'EL ARCHIVO CARGADO NO ES UNA IMAGEN'` });
       return;
     }
     this.img_files = $event.target.files[0];
@@ -82,7 +82,7 @@ export class AddNewProductComponent implements OnInit {
     reader.onloadend = () => this.img_previzualizar = reader.result;
   }
 
-  addImages(){
+  addImages() {
     this.images_files.push({
       file: this.img_files,
       show: this.img_previzualizar,
@@ -91,48 +91,48 @@ export class AddNewProductComponent implements OnInit {
     this.img_previzualizar = null;
   }
 
-  removeImages(index){
-    this.images_files.splice(index,1);
+  removeImages(index) {
+    this.images_files.splice(index, 1);
   }
 
-  createProduct(){
+  createProduct() {
 
-    if(! this.title ||
-      ! this.sku ||
-      ! this.categorie_id ||
-      ! this.price_soles ||
-      ! this.price_usd ||
-      ! this.resumen ||
-      ! this.description ||
-      ! this.imagen_file){
-        this.toaster.open(NoticyAlertComponent,{text:`danger-'TODOS LOS CAMPOS SON OBLIGATORIOS'`});
+    if (!this.title ||
+      !this.sku ||
+      !this.categorie_id ||
+      !this.price_soles ||
+      !this.price_usd ||
+      !this.resumen ||
+      !this.description ||
+      !this.imagen_file) {
+      this.toaster.open(NoticyAlertComponent, { text: `danger-'TODOS LOS CAMPOS SON OBLIGATORIOS'` });
       return;
     }
-    if(this.images_files.length == 0){
-      this.toaster.open(NoticyAlertComponent,{text:`danger-'DEBES INGRESAR UN CONJUNTO DE IMAGENES'`});
+    if (this.images_files.length == 0) {
+      this.toaster.open(NoticyAlertComponent, { text: `danger-'DEBES INGRESAR UN CONJUNTO DE IMAGENES'` });
       return;
     }
 
     let formaData = new FormData();
-    formaData.append("title",this.title);
-    formaData.append("sku",this.sku)
-    formaData.append("categorie_id",this.categorie_id)
-    formaData.append("price_soles",this.price_soles)
-    formaData.append("price_usd",this.price_usd)
-    formaData.append("resumen",this.resumen)
-    formaData.append("description",this.description)
-    formaData.append("imagen_file",this.imagen_file)
-    formaData.append("tags",this.tags)
+    formaData.append("title", this.title);
+    formaData.append("sku", this.sku)
+    formaData.append("categorie_id", this.categorie_id)
+    formaData.append("price_soles", this.price_soles)
+    formaData.append("price_usd", this.price_usd)
+    formaData.append("resumen", this.resumen)
+    formaData.append("description", this.description)
+    formaData.append("imagen_file", this.imagen_file)
+    formaData.append("tags", this.tags)
 
     let index = 0;
     for (const imagen of this.images_files) {
-      formaData.append("files["+index+"]",imagen.file);
+      formaData.append("files[" + index + "]", imagen.file);
       index++;
     }
 
-    this._productService.createProduct(formaData).subscribe((resp:any) => {
+    this._productService.createProduct(formaData).subscribe((resp: any) => {
       console.log(resp);
-      this.toaster.open(NoticyAlertComponent,{text:`primary-'EL PRODUCTO SE REGISTRO CORRECTAMENTE.'`});
+      this.toaster.open(NoticyAlertComponent, { text: `primary-'EL PRODUCTO SE REGISTRO CORRECTAMENTE.'` });
       this.title = null;
       this.sku = null;
       this.categorie_id = null;
@@ -145,5 +145,9 @@ export class AddNewProductComponent implements OnInit {
       this.tags = [];
       this.images_files = [];
     })
+  }
+
+  volver() {
+
   }
 }
