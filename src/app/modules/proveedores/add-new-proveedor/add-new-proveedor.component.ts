0a1e2b56f8d6f1cfbf0a16c2756d98a48ec4762a
Luis A. Guevara <guevaredo03@gmail.com>
2024-01-04 16:34:12 -0500

    add-sublista en memoria

--- src/app/modules/proveedores/add-new-proveedor/add-new-proveedor.component.ts
+++ src/app/modules/proveedores/add-new-proveedor/add-new-proveedor.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Toaster } from 'ngx-toast-notifications';
+import { NoticyAlertComponent } from 'src/app/componets/notifications/noticy-alert/noticy-alert.component';
 
 @Component({
   selector: 'app-add-new-proveedor',
@@ -7,9 +9,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddNewProveedorComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public toaster: Toaster,
+  ) { }
+
+  listContacto: any = [];
+
+  /* Data Contacto */
+  nombreContacto: any = null;
+  correoContacto: any = null;
+  tipoDocumentoContacto: any = 1;
+  nroDocumentoContacto: any = null;
+  celularContacto: any = null;
 
   ngOnInit(): void {
   }
 
+  addContacto() {
+
+    /* Validar campos llenos */
+    if (!this.nombreContacto ||
+      !this.correoContacto ||
+      !this.tipoDocumentoContacto ||
+      !this.celularContacto) {
+
+      this.toaster.open(NoticyAlertComponent, { text: `danger-'Todos los campos del contacto son obligatorios'` });
+      return;
+    }
+
+
+    /* Insertar campos a la lista */
+    let dataContacto = {
+      id: 0,
+      nombre: this.nombreContacto,
+      correo: this.correoContacto,
+      tipoDocumento: this.tipoDocumentoContacto,
+      nroDocumento: this.nroDocumentoContacto,
+      celular: this.celularContacto,
+
+    }
+
+    this.listContacto.push(dataContacto);
+
+    /* Resetear campos */
+
+    this.nombreContacto = null;
+    this.correoContacto = null;
+    this.nroDocumentoContacto = null;
+    this.celularContacto = null;
+
+
+  }
+
+  removeContacto(contacto: any) {
+
+    if (contacto.id == 0) {
+      this.listContacto = this.listContacto.filter((item) => item != contacto);
+    }
+
+
+  }
 }
