35b6496dc2e9bfdc34832bfeec8e47b47d1754d8
Strider <victor.villar2014@hotmail.com>
2024-01-30 21:13:30 -0500

    Funciones_de_List_Clientes

--- src/app/modules/clientes/list-cliente/list-cliente.component.ts
+++ src/app/modules/clientes/list-cliente/list-cliente.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { URL_BACKEND } from 'src/app/config/config';
 import { ClienteService } from '../_services/cliente.service';
+import { AddClienteComponent } from '../add-cliente/add-cliente.component';
+import { DeleteClienteComponent } from '../delete-cliente/delete-cliente.component';
 import { EditClienteComponent } from '../edit-cliente/edit-cliente.component';
 
 @Component({
@@ -33,6 +35,27 @@ export class ListClienteComponent implements OnInit {
     })
   }
 
+  reset(){
+    this.search = null;
+    this.allClientes();
+  }
+
+  addCliente(){
+    const modalRef = this.modelService.open(AddClienteComponent, {centered : true, size: 'lg'});
+    modalRef.result.then(
+      () => {
+
+      },
+      () => {
+        
+      }
+    )
+    modalRef.componentInstance.clientsE.subscribe((resp:any)=>{
+      // console.log(resp);
+      this.clientes.unshift(resp);
+    });
+  }
+
   edit(cliente){
     console.log(cliente)
     const modalRef = this.modelService.open(EditClienteComponent, {centered : true, size: 'lg'});
@@ -48,6 +71,23 @@ export class ListClienteComponent implements OnInit {
     modalRef.componentInstance.clientsE.subscribe((resp:any) => {
       let INDEX = this.clientes.findIndex(item => item.id == resp.id);
       this.clientes[INDEX] = resp;
+    })
+  }
+
+  delete(cliente){
+    const modalRef = this.modelService.open(DeleteClienteComponent, {centered : true, size: 'md'});
+    modalRef.componentInstance.cliente_selected = cliente;
+    modalRef.result.then(
+      () => {
+
+      },
+      () => {
+        
+      }
+    )
+    modalRef.componentInstance.clientsE.subscribe((resp:any) => {
+      let INDEX = this.clientes.findIndex(item => item.id == resp.id);
+      this.clientes.splice(INDEX,1);
     })
   }
   
