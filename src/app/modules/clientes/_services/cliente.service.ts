26063d3510779fea11d9dd46f2bdc2b8d2afe2dc
Luis A. Guevara <guevaredo03@gmail.com>
2024-02-03 14:08:37 -0500

    cboDepartamento y cboPais

--- src/app/modules/clientes/_services/cliente.service.ts
+++ src/app/modules/clientes/_services/cliente.service.ts
@@ -20,45 +20,67 @@ export class ClienteService {
     this.isLoading$ = this.isLoadingSubject.asObservable();
   }
 
-  allClientes(page=1,search=''){
+  allClientes(page = 1, search = '') {
     this.isLoadingSubject.next(true);
-    let headers = new HttpHeaders({'Authorization' : 'Bearer '+this.authservice.token});
+    let headers = new HttpHeaders({ 'Authorization': 'Bearer ' + this.authservice.token });
     let LINK = "";
-    if(search){
-      LINK = LINK + "&search="+search;
+    if (search) {
+      LINK = LINK + "&search=" + search;
     }
-    let URL = URL_SERVICIOS + "/clientes/all?page="+page+LINK;
-    return this.http.get(URL,{headers: headers}).pipe(
+    let URL = URL_SERVICIOS + "/clientes/all?page=" + page + LINK;
+    return this.http.get(URL, { headers: headers }).pipe(
       finalize(() => this.isLoadingSubject.next(false))
     );
   }
 
-  createCliente(data:any){
+  createCliente(data: any) {
     this.isLoadingSubject.next(true);
-    let headers = new HttpHeaders({'Authorization' : 'Bearer '+this.authservice.token});
+    let headers = new HttpHeaders({ 'Authorization': 'Bearer ' + this.authservice.token });
     let URL = URL_SERVICIOS + "/clientes/add";
-    return this.http.post(URL,data,{headers: headers}).pipe(
+    return this.http.post(URL, data, { headers: headers }).pipe(
       finalize(() => this.isLoadingSubject.next(false))
     );
   }
 
-  updateCliente(categorie_id:any, data:any){
+  updateCliente(categorie_id: any, data: any) {
     this.isLoadingSubject.next(true);
-    let headers = new HttpHeaders({'Authorization' : 'Bearer '+this.authservice.token});
-    let URL = URL_SERVICIOS + "/clientes/update/"+categorie_id;
-    return this.http.post(URL,data,{headers: headers}).pipe(
+    let headers = new HttpHeaders({ 'Authorization': 'Bearer ' + this.authservice.token });
+    let URL = URL_SERVICIOS + "/clientes/update/" + categorie_id;
+    return this.http.post(URL, data, { headers: headers }).pipe(
       finalize(() => this.isLoadingSubject.next(false))
     );
   }
-  deleteCliente(cliente_id:any){
+  deleteCliente(cliente_id: any) {
     this.isLoadingSubject.next(true);
-    let headers = new HttpHeaders({'Authorization' : 'Bearer '+this.authservice.token});
-    let URL = URL_SERVICIOS + "/clientes/delete/"+cliente_id;
-    return this.http.delete(URL,{headers: headers}).pipe(
+    let headers = new HttpHeaders({ 'Authorization': 'Bearer ' + this.authservice.token });
+    let URL = URL_SERVICIOS + "/clientes/delete/" + cliente_id;
+    return this.http.delete(URL, { headers: headers }).pipe(
       finalize(() => this.isLoadingSubject.next(false))
     );
   }
 
 
+
+  getPaises() {
+    this.isLoadingSubject.next(true);
+    let headers = new HttpHeaders({ 'Authorization': 'Bearer ' + this.authservice.token });
+    let LINK = "";
+
+    let URL = URL_SERVICIOS + "/paises/all";
+    return this.http.get(URL, { headers: headers }).pipe(
+      finalize(() => this.isLoadingSubject.next(false))
+    );
+  }
+
+  getDepartamento(idPais: number) {
+    this.isLoadingSubject.next(true);
+    let headers = new HttpHeaders({ 'Authorization': 'Bearer ' + this.authservice.token });
+    let LINK = "";
+
+    let URL = URL_SERVICIOS + "/paises/show/" + idPais;
+    return this.http.get(URL, { headers: headers }).pipe(
+      finalize(() => this.isLoadingSubject.next(false))
+    );
+  }
 
 }
--- src/app/modules/clientes/add-cliente/add-cliente.component.ts
+++ src/app/modules/clientes/add-cliente/add-cliente.component.ts
@@ -3,6 +3,7 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { Toaster } from 'ngx-toast-notifications';
 import { NoticyAlertComponent } from 'src/app/componets/notifications/noticy-alert/noticy-alert.component';
 import { ClienteService } from '../_services/cliente.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-add-cliente',
@@ -12,22 +13,26 @@ import { ClienteService } from '../_services/cliente.service';
 export class AddClienteComponent implements OnInit {
 
   @Output() clientsE: EventEmitter<any> = new EventEmitter();
-  isLoading$:any;
-  isLoading:boolean = false;
+  isLoading$: any;
+  isLoading: boolean = false;
 
   //Datos Cliente
-  nombres:any = null;
-  apellidos:any = null;
-  correo:any = null;
-  nroDocumento:any = null;
-  contacto:any=null;
-  tipoPersona:any =null;
-  pais:any = null;
-  departamento:any=null;
-  direccion:any=null;
-  observacion:any=null;
- 
-  
+  nombres: any = null;
+  apellidos: any = null;
+  correo: any = null;
+  nroDocumento: any = null;
+  contacto: any = null;
+  tipoPersona: any = null;
+  pais: any = null;
+  departamento: any = null;
+  direccion: any = null;
+  observacion: any = null;
+  lstPaises: any = [];
+
+  cboPais = new FormControl();
+  cboDepartamento = new FormControl();
+  lstDepartamentos: any = [];
+
   constructor(
     public _clienteService: ClienteService,
     public modal: NgbActiveModal,
@@ -36,36 +41,64 @@ export class AddClienteComponent implements OnInit {
 
   ngOnInit(): void {
     this.isLoading$ = this._clienteService.isLoading$;
+
+
+    setTimeout(() => {
+      this.getPaises();
+      this.getDepartamento(89);
+      this.cboDepartamento.setValue(this.lstDepartamentos[0]);
+    }, 10);
+
+    this.cboPais.valueChanges.subscribe(value => {
+      this.getDepartamento(value);
+    });
+
   }
 
-  
+  save() {
 
-  save(){
-    
 
-    let datacliente = 
+    let datacliente =
     {
-      "nombres" : this.nombres,
-      "apellidos" : this.apellidos,
+      "nombres": this.nombres,
+      "apellidos": this.apellidos,
       "correo": this.correo,
-      "nroDocumento":this.nroDocumento,
-      "estado" : 1,
-      "contacto" : this.contacto,
+      "nroDocumento": this.nroDocumento,
+      "estado": 1,
+      "contacto": this.contacto,
       "tipoPersona": this.tipoPersona,
-      "pais" : this.pais,
-      "departamento" : this.departamento,
-      "direccion" : this.direccion,
-      "observacion": this.observacion 
+      "pais": this.cboPais.value,
+      "departamento": this.cboDepartamento.value,
+      "direccion": this.direccion,
+      "observacion": this.observacion
     };
 
-    
-    
-    this._clienteService.createCliente(datacliente).subscribe((resp:any) => {
+
+
+    this._clienteService.createCliente(datacliente).subscribe((resp: any) => {
       console.log(resp);
       this.clientsE.emit(resp.categorie);
-      this.toaster.open(NoticyAlertComponent,{text:`primary-'EL CLIENTE SE A REGISTRADO DE MANERA CORRECTA.'`});
+      this.toaster.open(NoticyAlertComponent, { text: `primary-'EL CLIENTE SE A REGISTRADO DE MANERA CORRECTA.'` });
       this.modal.close();
     })
   }
+
+  getPaises() {
+    this._clienteService.getPaises().subscribe((resp: any) => {
+      console.log(resp);
+      this.lstPaises = resp.paises;
+      this.cboPais.setValue(89); //perú
+
+    })
+  }
+  getDepartamento(idPais: number) {
+    this._clienteService.getDepartamento(idPais).subscribe((resp: any) => {
+      console.log(resp);
+      this.lstDepartamentos = resp.ciudades;
+      this.cboDepartamento.setValue(this.lstDepartamentos[0]); //Amazonas
+
+    })
+  }
+
 
 }
